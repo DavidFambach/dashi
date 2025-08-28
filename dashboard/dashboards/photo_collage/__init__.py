a432fba7ec861ee17ac180365e1faf897e521d26
DavidFambach <david.fambach@gmx.de>
2025-08-28 10:46:47 +0200

    Made photo_collage private and removed the upload funktion

--- dashboard/dashboards/photo_collage/__init__.py
+++ dashboard/dashboards/photo_collage/__init__.py
@@ -1,8 +1,7 @@
 import shutil
 import os
-from random import randint
 
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, render_template
 
 # Define a Blueprint for the Party Collage dashboard
 blueprint = Blueprint('photo_collage', __name__,
@@ -15,21 +14,6 @@ if os.path.exists(temp_data_dir):
     shutil.rmtree(temp_data_dir)
 os.mkdir(temp_data_dir)
 
-# Dummy photos (replace with real storage later)
-
-
 @blueprint.route('/')
 def dashboard():
     return render_template('photo_collage.html')
-
-@blueprint.route('/api/photos')
-def api_photos():
-    DUMMY_PHOTOS = []
-    for i in range(randint(6, 30)):
-        DUMMY_PHOTOS.append("https://picsum.photos/400?random=" + str(i))
-
-    return jsonify(DUMMY_PHOTOS)
-
-@blueprint.route('/upload')
-def upload_page():
-    return render_template('photo_upload.html')
